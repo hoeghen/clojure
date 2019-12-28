6e8816103a39db152ced2bf94458a5a996d64e10
hoeghen <carverdk@yahoo.dk>
2019-12-28 14:49:17 +0100

    added delete to firebase

--- src/minlokalebutik/firebase.clj
+++ src/minlokalebutik/firebase.clj
@@ -4,17 +4,31 @@
   (:require [cheshire.core :refer :all])
   (:require [clojure.string :as str])
   (:require [pandect.algo.sha256 :refer :all])
-  (:require [clojure.pprint :refer [pprint print-table]]))
+  (:require [clojure.pprint :refer [pprint print-table]])
+  (:require [minlokalebutik.time  :refer :all]))
+
 
 (defn postTilbud [tilbud]
   (client/post "https://minlokalebutik.firebaseio.com/alletilbud.json" {:form-params tilbud
                                                                         :content-type :json}))
 
 (defn load-tilbud [parms]
-  (parse-string (:body (client/get (str "https://minlokalebutik.firebaseio.com/alletilbud.json") {
-                                                                                                  :debug true}))))
+  (parse-string (:body (client/get (str "https://minlokalebutik.firebaseio.com/alletilbud.json" ) {:query-params parms
+                                                                                                   :debug true}))))
+
+(defn load-tilbud-before [date]
+  (let [earlier {:orderBy "\"slut\""
+                  :endAt (str "\"" date "\"")}]
+    (load-tilbud earlier)))
 
 
+(defn delete-tilbud [key]
+  (client/delete (str "https://minlokalebutik.firebaseio.com/alletilbud/" key ".json") {:debug true}))
+
+
+
+(defn delete-tilbud-before [date]
+  (map delete-tilbud (keys (load-tilbud-before date))))
 
 
 
--- src/minlokalebutik/main.clj
+++ src/minlokalebutik/main.clj
@@ -1,13 +1,18 @@
 (ns minlokalebutik.main
   (:gen-class)
   (:require [minlokalebutik.shopgun :refer :all])
-  (:require [minlokalebutik.firebase :refer :all]))
+  (:require [minlokalebutik.firebase :refer :all])
+  (:require [minlokalebutik.time :refer :all]))
 
 
 (defn -main
   [& args]
   (println "calling shopgun and returning all offers in a postcode")
-  (map postTilbud (load-all-today)))
+  ;; load tilbud fra idag
+  (map postTilbud (load-all-today))
+
+  (delete-tilbud-before (.toString (today))))
 
 
-(-main)
+
+
